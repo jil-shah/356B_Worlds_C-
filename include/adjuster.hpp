4ee63f958718e39209091703327bd60268d3644c
jilshah2001 <33742487+jilshah2001@users.noreply.github.com>
2020-12-15 19:00:04 -0500

    Create adjuster.hpp

---
+++ include/adjuster.hpp
@@ -0,0 +1,14 @@
+#ifndef _ADJUSTER_H_
+#define _ADJUSTER_H_
+
+void adjust(int vel);
+
+void _adjust(int distance, int vel);
+
+void adjustReset();
+
+void change_angle();
+
+void adjusterOp(void* parameter);
+
+#endif
